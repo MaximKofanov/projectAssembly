83ee0ea14a2574abe4bce922ab5a46b61ecbe281
MaximKofanov <m.kofanov@gmail.com>
2017-09-10 17:18:09 +0300

    varsion 0.0.3

--- projectAssembly.js
+++ projectAssembly.js
@@ -4,6 +4,12 @@ $(function(){
 	// > подгрузить библиотеки 1-ая jquery, 2-ая эта остальные после > Добавить нужным скриптам класс "myScript" >
 	// > заполнить обьект templateSettings классами старой и новой цены
 
+  // <script src="http://cdn.shakesine.com/js/jquery.js" class="myScript"></script>
+  // <script src="projectAssembly.js" class="myScript"></script>
+  // <script src="http://cdn.shakesine.com/js/countrieslist_990cis.js" class="myScript"></script>   
+  // <script src="http://cdn.shakesine.com/js/countries.js" class="myScript"></script>  
+  // <script src="http://cdn.shakesine.com/js/shakesland.js" class="myScript"></script>
+
 	// Данный код подключается после jquery, но до нужных скриптов!
 	// Ненужное отключить '//'.
 	// Реализованно:
@@ -63,231 +69,231 @@ $(function(){
 	//(function(d,s){s=d.createElement('script');s.src='https://code.jquery.com/jquery-3.1.0.js';(d.head||d.documentElement).appendChild(s)})(document);
 
 	//Начало скрипта
-  var counterElementsImageDiv = $('div').length;
-  var counterElementsImageSpan = $('span').length;
-  var counterElementsImageSection = $('section').length;
-  var counterElementsImageLi = $('li').length;
-  var counterElementsImageUl = $('ul').length;
-  var counterElementsImageA = $('a').length;
-  var counterElementsImageP = $('p').length;
-  var counterElementsImageHeader = $('Header').length;
-  var counterElementsImageFooter = $('Footer').length;
-  var counterElementsImageI = $('i').length;
-  var arrLinksImage = [];
-  var LinkImage;
-  var counterImageIteration = 0;
-  //Убрать повторяющиеся картинки в готовом массиве
-  function removeRepetitionsImg(){
-    for(var i = 0; i < arrLinksImage.length; i++){
-      for(var j = i+1; j < arrLinksImage.length; j++){
-        if(arrLinksImage[i] === arrLinksImage[j]){
-          arrLinksImage.splice(j,1);
-          i--;
-          j--;
-        }
-      }
-    }
-  } 
-  //Картинки в DIV
-  for(var i = 0; i <= counterElementsImageDiv; i++){
-    LinkImage = $('div').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageDiv; i++){
-    LinkImage = $('div').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в SPAN
-  for(var i = 0; i <= counterElementsImageSpan; i++){
-    LinkImage = $('span').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageSpan; i++){
-    LinkImage = $('span').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в SPAN
-  for(var i = 0; i <= counterElementsImageSection; i++){
-    LinkImage = $('Section').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageSection; i++){
-    LinkImage = $('Section').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в LI
-  for(var i = 0; i <= counterElementsImageLi; i++){
-    LinkImage = $('li').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageLi; i++){
-    LinkImage = $('li').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в UL
-  for(var i = 0; i <= counterElementsImageUl; i++){
-    LinkImage = $('ul').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageUl; i++){
-    LinkImage = $('ul').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в A
-  for(var i = 0; i <= counterElementsImageA; i++){
-    LinkImage = $('a').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageA; i++){
-    LinkImage = $('a').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в P
-  for(var i = 0; i <= counterElementsImageP; i++){
-    LinkImage = $('p').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageP; i++){
-    LinkImage = $('p').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в Header
-  for(var i = 0; i <= counterElementsImageHeader; i++){
-    LinkImage = $('Header').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageHeader; i++){
-    LinkImage = $('Header').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в Footer
-  for(var i = 0; i <= counterElementsImageFooter; i++){
-    LinkImage = $('Footer').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageFooter; i++){
-    LinkImage = $('Footer').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  //Картинки в i
-  for(var i = 0; i <= counterElementsImageI; i++){
-    LinkImage = $('i').eq(i).css('background-image');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  for(var i = 0; i <= counterElementsImageI; i++){
-    LinkImage = $('i').eq(i).css('background');
-      if(typeof LinkImage !== "string"){
-        continue;
-      }else if(LinkImage.slice(0,3) === 'url'){
-        arrLinksImage.push(LinkImage.slice(5,-2));
-      }
-  }
-  removeRepetitionsImg();		
+  // var counterElementsImageDiv = $('div').length;
+  // var counterElementsImageSpan = $('span').length;
+  // var counterElementsImageSection = $('section').length;
+  // var counterElementsImageLi = $('li').length;
+  // var counterElementsImageUl = $('ul').length;
+  // var counterElementsImageA = $('a').length;
+  // var counterElementsImageP = $('p').length;
+  // var counterElementsImageHeader = $('Header').length;
+  // var counterElementsImageFooter = $('Footer').length;
+  // var counterElementsImageI = $('i').length;
+  // var arrLinksImage = [];
+  // var LinkImage;
+  // var counterImageIteration = 0;
+  // //Убрать повторяющиеся картинки в готовом массиве
+  // function removeRepetitionsImg(){
+  //   for(var i = 0; i < arrLinksImage.length; i++){
+  //     for(var j = i+1; j < arrLinksImage.length; j++){
+  //       if(arrLinksImage[i] === arrLinksImage[j]){
+  //         arrLinksImage.splice(j,1);
+  //         i--;
+  //         j--;
+  //       }
+  //     }
+  //   }
+  // } 
+  // //Картинки в DIV
+  // for(var i = 0; i <= counterElementsImageDiv; i++){
+  //   LinkImage = $('div').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageDiv; i++){
+  //   LinkImage = $('div').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в SPAN
+  // for(var i = 0; i <= counterElementsImageSpan; i++){
+  //   LinkImage = $('span').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageSpan; i++){
+  //   LinkImage = $('span').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в SPAN
+  // for(var i = 0; i <= counterElementsImageSection; i++){
+  //   LinkImage = $('Section').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageSection; i++){
+  //   LinkImage = $('Section').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в LI
+  // for(var i = 0; i <= counterElementsImageLi; i++){
+  //   LinkImage = $('li').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageLi; i++){
+  //   LinkImage = $('li').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в UL
+  // for(var i = 0; i <= counterElementsImageUl; i++){
+  //   LinkImage = $('ul').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageUl; i++){
+  //   LinkImage = $('ul').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в A
+  // for(var i = 0; i <= counterElementsImageA; i++){
+  //   LinkImage = $('a').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageA; i++){
+  //   LinkImage = $('a').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в P
+  // for(var i = 0; i <= counterElementsImageP; i++){
+  //   LinkImage = $('p').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageP; i++){
+  //   LinkImage = $('p').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в Header
+  // for(var i = 0; i <= counterElementsImageHeader; i++){
+  //   LinkImage = $('Header').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageHeader; i++){
+  //   LinkImage = $('Header').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в Footer
+  // for(var i = 0; i <= counterElementsImageFooter; i++){
+  //   LinkImage = $('Footer').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageFooter; i++){
+  //   LinkImage = $('Footer').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // //Картинки в i
+  // for(var i = 0; i <= counterElementsImageI; i++){
+  //   LinkImage = $('i').eq(i).css('background-image');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // for(var i = 0; i <= counterElementsImageI; i++){
+  //   LinkImage = $('i').eq(i).css('background');
+  //     if(typeof LinkImage !== "string"){
+  //       continue;
+  //     }else if(LinkImage.slice(0,3) === 'url'){
+  //       arrLinksImage.push(LinkImage.slice(5,-2));
+  //     }
+  // }
+  // removeRepetitionsImg();		
 
-  //Кнопка для скачивания картинок
-  var counterImageForDownload = 0; 
-  $('body').prepend('<a href="" download="" class="downloadImages"><div>Осталось скачать картинок: <span></span></div></a>') 
-  $('.downloadImages').css({
-  	width:'300px',
-  	height:'100px',
-  	backgroundColor: 'red',
-  	position: 'fixed',
-  	top: '10px',
-  	left: '10px',
-  	zIndex: '1000',
-  	borderRadius: '10px',
-  	textAlign: 'center',
-  	color: '#fff',
-  	fontSize: '18px',
-  	lineHeight: '4.5'
-  });
+  // //Кнопка для скачивания картинок
+  // var counterImageForDownload = 0; 
+  // $('body').prepend('<a href="" download="" class="downloadImages"><div>Осталось скачать картинок: <span></span></div></a>') 
+  // $('.downloadImages').css({
+  // 	width:'300px',
+  // 	height:'100px',
+  // 	backgroundColor: 'red',
+  // 	position: 'fixed',
+  // 	top: '10px',
+  // 	left: '10px',
+  // 	zIndex: '1000',
+  // 	borderRadius: '10px',
+  // 	textAlign: 'center',
+  // 	color: '#fff',
+  // 	fontSize: '18px',
+  // 	lineHeight: '4.5'
+  // });
 
-  $('.downloadImages').on('click', function(){
-     if(counterImageForDownload < arrLinksImage.length){
-       	$('.downloadImages').attr('href', arrLinksImage[counterImageForDownload]);
-      	counterImageForDownload++;
-     }else{
-     		$('.downloadImages').css('display','none');
-       	counterImageForDownload = 0;
-     }
-    $('.downloadImages div span').html(arrLinksImage.length - counterImageForDownload);  
-  });
+  // $('.downloadImages').on('click', function(){
+  //    if(counterImageForDownload < arrLinksImage.length){
+  //      	$('.downloadImages').attr('href', arrLinksImage[counterImageForDownload]);
+  //     	counterImageForDownload++;
+  //    }else{
+  //    		$('.downloadImages').css('display','none');
+  //      	counterImageForDownload = 0;
+  //    }
+  //   $('.downloadImages div span').html(arrLinksImage.length - counterImageForDownload);  
+  // });
   //Конец скрипта
 
 
